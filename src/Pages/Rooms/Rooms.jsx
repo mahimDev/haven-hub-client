295e9339afffc59489bb17b52e5c6615d70aad96
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-02-11 18:14:47 +0600

    added sorting functionality

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet";
 import axios from "axios";
 
 const Rooms = () => {
-
+    const [sorting, setSorting] = useState("")
     const [rooms, setRooms] = useState([])
 
     const [min, setMin] = useState('')
@@ -23,21 +23,26 @@ const Rooms = () => {
         setMax(maxValue);
     };
     useEffect(() => {
-
         axios
-            .get(`https://hotel-booking-server-sable.vercel.app/rooms?min=${min}&max=${max}`)
+            .get(`https://hotel-booking-server-sable.vercel.app/rooms?min=${min}&max=${max}&sorting=${sorting}`)
             .then((res) => setRooms(res.data))
             .catch(() => {
 
             });
 
-    }, [min, max]);
+    }, [min, max, sorting]);
+    console.log(sorting)
     return (
         <div className="max-w-[1440px] mx-auto mt-10 min-h-[80vh]">
             <Helmet>
                 <title>Rooms | HavenHub</title>
             </Helmet>
-            <div className="flex justify-end">
+            <div className="md:flex justify-between">
+                <select onChange={(e) => setSorting(e.target.value)} className="text-darkGray border py-2 rounded pl-2 mb-4  md:mb-0">
+                    <option disabled>Select</option>
+                    <option value="ascending">ascending price</option>
+                    <option value="descending">descending price</option>
+                </select>
                 <form
                     onSubmit={handleBtn}
                 >
@@ -58,7 +63,7 @@ const Rooms = () => {
                     <button className="py-2 px-5 border rounded-r bg-darkGray text-lightGray font-semibold">Filter </button>
                 </form>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  ">
                 {rooms?.map(room => <RoomCart key={room._id} room={room}></RoomCart>)}
             </div>
         </div>
